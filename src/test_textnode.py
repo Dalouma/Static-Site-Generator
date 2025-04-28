5ce68edbbf42cd04f939fafd007371e3a2ed7c83
Dalouma <david.amaya20@yahoo.com>
2025-04-28 00:53:50 -0400

    added unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_capitalization(self):
+        node = TextNode("Capitalized or Not", TextType.NORMAL)
+        node2 = TextNode("capitalized or not", TextType.NORMAL)
+        self.assertNotEqual(node, node2)
+
+    def test_link_absence(self):
+        node = TextNode("random text", TextType.NORMAL)
+        node2 = TextNode("random text", TextType.NORMAL, "")
+        self.assertNotEqual(node, node2)
+
+    def test_text_type_diff(self):
+        node = TextNode("random text", TextType.NORMAL)
+        node2 = TextNode("random text", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_NONE_url(self):
+        node = TextNode("random text", TextType.NORMAL)
+        node2 = TextNode("random text", TextType.NORMAL, None)
+        self.assertEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
