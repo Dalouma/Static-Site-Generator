96737e762f5a6a3cb818f9a963ba32df9c35eb18
Dalouma <david.amaya20@yahoo.com>
2025-04-28 16:00:40 -0400

    Added LeafNode class and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,9 +11,19 @@ class HTMLNode():
         raise NotImplementedError()
     
     def props_to_html(self):
-        return " " + " ".join([f'{key}="{value}"' for key, value in self.props.items()])
+        return " " + " ".join([f'{key}="{value}"' for key, value in self.props.items()]) if self.props else ""
 
     def __repr__(self):
         return f"HTMLNode(tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
     
+    def to_html(self):
+        if self.value is None:
+            raise ValueError()
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>"
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,22 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
         node = HTMLNode("p", "This is a paragraph.", None, {"href": "randomlink.com", "target": "_blank"})
         self.assertEqual(node.props_to_html()[0], " ")
+        self.assertEqual(repr(node), "HTMLNode(tag: p, value: This is a paragraph., children: None, props: {'href': 'randomlink.com', 'target': '_blank'})")
+    
+    def test_no_props(self):
+        node = HTMLNode("p", "This is a paragraph.")
+        self.assertEqual(node.props_to_html(), "")
+    
+    def test_empty_node(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+        self.assertEqual(repr(node), "HTMLNode(tag: None, value: None, children: None, props: None)")
 
     def test_repr(self):
         node = HTMLNode("p", "This is a paragraph.", None, {"href": "randomlink.com", "target": "_blank"})
@@ -23,7 +33,22 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIn("tag: h1", node_string)
         self.assertIn("value: Title Goes Here", node_string)
         self.assertIn("props: {'href': 'randomlink.com'}", node_string)
-        
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "Hello, world!")
+        self.assertEqual(node.to_html(), "<h1>Hello, world!</h1>")
+
+    def test_leaf_to_html_hyperlink(self):
+        node = LeafNode("a", "Website Link", {'href': 'https://example.com'})
+        self.assertEqual(node.to_html(), '<a href="https://example.com">Website Link</a>')
+
+    def test_leaf_to_html_image(self):
+        node = LeafNode("img", "", {"src": "/example/filepath/orange.png", "alt": "An orange"})
+        self.assertEqual(node.to_html(), '<img src="/example/filepath/orange.png" alt="An orange"></img>')
 
 
 if __name__ == "__main__":
